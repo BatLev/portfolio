52b72b224f53757f728cca5e0deb94dfd9fe4f3c
vortex <batoglevente@gmail.hu>
2022-05-30 15:59:02 +0200

    pret

--- src/routes/aboutMe/AboutMe.jsx
+++ src/routes/aboutMe/AboutMe.jsx
@@ -11,7 +11,6 @@ export default function aboutMe() {
                     <h1>About me</h1>
                     <img src='https://rune.ai/images/RuneLogoPurpleHD.png' />
                     <img style={{ top: '40rem', right: 'auto', left: '-25rem' }} src='https://rune.ai/images/RuneLogoPurpleHD.png' />
-                    <img src="https://nakedsecurity.sophos.com/wp-content/uploads/sites/2/2013/08/facebook-silhouette_thumb.jpg" />
                 </div>
             </main>
         </div>
